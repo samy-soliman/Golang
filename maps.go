9b4415eb676c0f2e08c9333de69fbfe5ebf6a24c
samy-soliman <samy.soliman1997@gmail.com>
2023-12-27 16:03:34 +0200

    last of maps notes for now

--- maps.go
+++ maps.go
@@ -191,11 +191,12 @@ func main() {
 */
 
 /*
-Key Types
+Key Types:
 
 Any type can be used as the value in a map, but keys are more restrictive.
-
-As mentioned earlier, map keys may be of any type that is comparable. The language spec defines this precisely, but in short, comparable types are boolean, numeric, string, pointer, channel, and interface types, and structs or arrays that contain only those types. Notably absent from the list are slices, maps, and functions; these types cannot be compared using ==, and may not be used as map keys.
+As mentioned earlier, map keys may be of any type that is comparable.
+The language spec defines this precisely, but in short, comparable types are boolean, numeric, string, pointer, channel, and interface types, and structs or arrays that contain only those types.
+Notably absent from the list are slices, maps, and functions; these types cannot be compared using ==, and may not be used as map keys.
 
 It's obvious that strings, ints, and other basic types should be available as map keys, but perhaps unexpected are struct keys. Struct can be used to key data by multiple dimensions. For example, this map of maps could be used to tally web page hits by country:
 
@@ -231,4 +232,169 @@ hits[Key{"/", "vn"}]++
 And it’s similarly straightforward to see how many Swiss people have read the spec:
 
 n := hits[Key{"/ref/spec", "ch"}]
+*/
+/*
+package main
+
+import (
+	"crypto/md5"
+	"fmt"
+	"io"
+)
+
+func getCounts(userIDs []string) map[string]int {
+	m := map[string]int{}
+	for _, id := range userIDs {
+		if _, ok := m[id]; !ok {
+			m[id] = 0
+		}
+		m[id]++
+	}
+	return m
+}
+
+
+func test(userIDs []string, ids []string) {
+	fmt.Printf("Generating counts for %v user IDs...\n", len(userIDs))
+
+	counts := getCounts(userIDs)
+	fmt.Println("Counts from select IDs:")
+	for _, k := range ids {
+		v := counts[k]
+		fmt.Printf(" - %s: %d\n", k, v)
+	}
+	fmt.Println("=====================================")
+}
+
+func main() {
+	userIDs := []string{}
+	for i := 0; i < 10000; i++ {
+		h := md5.New()
+		io.WriteString(h, fmt.Sprint(i))
+		key := fmt.Sprintf("%x", h.Sum(nil))
+		userIDs = append(userIDs, key[:2])
+	}
+
+	test(userIDs, []string{"00", "ff", "dd"})
+	test(userIDs, []string{"aa", "12", "32"})
+	test(userIDs, []string{"bb", "33"})
+}
+*/
+
+/*
+Like slices, maps hold references
+
+Like slices, maps hold references to an underlying data structure. If you pass a map to a function that changes the contents of the map, the changes will be visible in the caller.
+Map literals
+
+Maps can be constructed using the usual composite literal syntax with colon-separated key-value pairs, so it's easy to build them during initialization.
+
+var timeZone = map[string]int{
+    "UTC":  0*60*60,
+    "EST": -5*60*60,
+    "CST": -6*60*60,
+    "MST": -7*60*60,
+    "PST": -8*60*60,
+}
+
+Missing keys
+
+An attempt to fetch a map value with a key that is not present in the map will return the zero value for the type of the entries in the map.
+For instance, if the map contains integers, looking up a non-existent key will return 0.
+A set can be implemented as a map with value type bool. Set the map entry to true to put the value in the set, and then test it by simple indexing.
+
+attended := map[string]bool{
+    "Ann": true,
+    "Joe": true,
+    ...
+}
+
+if attended[person] { // will be false if person is not in the map
+    fmt.Println(person, "was at the meeting")
+}
+
+Sometimes you need to distinguish a missing entry from a zero value.
+Is there an entry for "UTC" or is that 0 because it's not in the map at all? You can discriminate with a form of multiple assignment.
+
+var seconds int
+var ok bool
+seconds, ok = timeZone[tz]
+
+For obvious reasons, this is called the “comma ok” idiom. In this example,
+if tz is present, seconds will be set appropriately and ok will be true; if not, seconds will be set to zero and ok will be false. Here's a function that puts it together with a nice error report:
+
+func offset(tz string) int {
+    if seconds, ok := timeZone[tz]; ok {
+        return seconds
+    }
+    log.Println("unknown time zone:", tz)
+    return 0
+}
+
+Deleting map entries
+To delete a map entry, use the delete built-in function, whose arguments are the map and the key to be deleted. It's safe to do this even if the key is already absent from the map.
+
+delete(timeZone, "PDT")  // Now on Standard Time
+*/
+
+/*
+Maps can contain maps, creating a nested structure. For example:
+
+map[string]map[string]int
+map[rune]map[string]int
+map[int]map[string]map[string]int
+*/
+/*
+package main
+
+import (
+	"fmt"
+)
+
+func getNameCounts(names []string) map[rune]map[string]int {
+	nameCounts := make(map[rune]map[string]int)
+	for _, name := range names {
+		firstLetter := rune(0)
+		if len(name) != 0 {
+			firstLetter = rune(name[0])
+		}
+
+		if _, ok := nameCounts[firstLetter]; !ok {
+			nameCounts[firstLetter] = make(map[string]int)
+		}
+		if _, ok := nameCounts[firstLetter][name]; !ok {
+			nameCounts[firstLetter][name] = 0
+		}
+		nameCounts[firstLetter][name]++
+	}
+	return nameCounts
+}
+
+func test(names []string, initial rune, name string) {
+	fmt.Printf("Generating counts for %v names...\n", len(names))
+
+	nameCounts := getNameCounts(names)
+	count := nameCounts[initial][name]
+	fmt.Printf("Count for [%c][%s]: %d\n", initial, name, count)
+	fmt.Println("=====================================")
+}
+
+func main() {
+	test(getNames(50), 'M', "Matthew")
+	test(getNames(100), 'G', "George")
+	test(getNames(150), 'D', "Drew")
+	test(getNames(200), 'P', "Philip")
+	test(getNames(250), 'B', "Bryant")
+	test(getNames(300), 'M', "Matthew")
+}
+
+func getNames(length int) []string {
+	names := []string{
+		"Grant", "Eduardo", "Peter", "Matthew", "Matthew", "Matthew", "Peter", "Peter", "Henry", "Parker", "Parker", "Parker", "Collin", "Hayden", "George", "Bradley", "Mitchell", "Devon", "Ricardo", "Shawn", "Taylor", "Nicolas", "Gregory", "Francisco", "Liam", "Kaleb", "Preston", "Erik", "Alexis", "Owen", "Omar", "Diego", "Dustin", "Corey", "Fernando", "Clayton", "Carter", "Ivan", "Jaden", "Javier", "Alec", "Johnathan", "Scott", "Manuel", "Cristian", "Alan", "Raymond", "Brett", "Max", "Drew", "Andres", "Gage", "Mario", "Dawson", "Dillon", "Cesar", "Wesley", "Levi", "Jakob", "Chandler", "Martin", "Malik", "Edgar", "Sergio", "Trenton", "Josiah", "Nolan", "Marco", "Drew", "Peyton", "Harrison", "Drew", "Hector", "Micah", "Roberto", "Drew", "Brady", "Erick", "Conner", "Jonah", "Casey", "Jayden", "Edwin", "Emmanuel", "Andre", "Phillip", "Brayden", "Landon", "Giovanni", "Bailey", "Ronald", "Braden", "Damian", "Donovan", "Ruben", "Frank", "Gerardo", "Pedro", "Andy", "Chance", "Abraham", "Calvin", "Trey", "Cade", "Donald", "Derrick", "Payton", "Darius", "Enrique", "Keith", "Raul", "Jaylen", "Troy", "Jonathon", "Cory", "Marc", "Eli", "Skyler", "Rafael", "Trent", "Griffin", "Colby", "Johnny", "Chad", "Armando", "Kobe", "Caden", "Marcos", "Cooper", "Elias", "Brenden", "Israel", "Avery", "Zane", "Zane", "Zane", "Zane", "Dante", "Josue", "Zackary", "Allen", "Philip", "Mathew", "Dennis", "Leonardo", "Ashton", "Philip", "Philip", "Philip", "Julio", "Miles", "Damien", "Ty", "Gustavo", "Drake", "Jaime", "Simon", "Jerry", "Curtis", "Kameron", "Lance", "Brock", "Bryson", "Alberto", "Dominick", "Jimmy", "Kaden", "Douglas", "Gary", "Brennan", "Zachery", "Randy", "Louis", "Larry", "Nickolas", "Albert", "Tony", "Fabian", "Keegan", "Saul", "Danny", "Tucker", "Myles", "Damon", "Arturo", "Corbin", "Deandre", "Ricky", "Kristopher", "Lane", "Pablo", "Darren", "Jarrett", "Zion", "Alfredo", "Micheal", "Angelo", "Carl", "Oliver", "Kyler", "Tommy", "Walter", "Dallas", "Jace", "Quinn", "Theodore", "Grayson", "Lorenzo", "Joe", "Arthur", "Bryant", "Roman", "Brent", "Russell", "Ramon", "Lawrence", "Moises", "Aiden", "Quentin", "Jay", "Tyrese", "Tristen", "Emanuel", "Salvador", "Terry", "Morgan", "Jeffery", "Esteban", "Tyson", "Braxton", "Branden", "Marvin", "Brody", "Craig", "Ismael", "Rodney", "Isiah", "Marshall", "Maurice", "Ernesto", "Emilio", "Brendon", "Kody", "Eddie", "Malachi", "Abel", "Keaton", "Jon", "Shaun", "Skylar", "Ezekiel", "Nikolas", "Santiago", "Kendall", "Axel", "Camden", "Trevon", "Bobby", "Conor", "Jamal", "Lukas", "Malcolm", "Zackery", "Jayson", "Javon", "Roger", "Reginald", "Zachariah", "Desmond", "Felix", "Johnathon", "Dean", "Quinton", "Ali", "Davis", "Gerald", "Rodrigo", "Demetrius", "Billy", "Rene", "Reece", "Kelvin", "Leo", "Justice", "Chris", "Guillermo", "Matthew", "Matthew", "Matthew", "Kevon", "Steve", "Frederick", "Clay", "Weston", "Dorian", "Hugo", "Roy", "Orlando", "Terrance", "Kai", "Khalil", "Khalil", "Khalil", "Graham", "Noel", "Willie", "Nathanael", "Terrell", "Tyrone",
+	}
+	if length > len(names) {
+		length = len(names)
+	}
+	return names[:length]
+}
 */
